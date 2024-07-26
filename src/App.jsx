1cf19d4ad7c0fc86b6d9cd952ff3e1ee276fc3cc
Tiffany Gong <gongt108@gmail.com>
2024-07-25 22:05:35 -0500

    add back to top btn

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,34 @@ function App() {
 				<section className="flex flex-col h-screen bg-slate-700 snap-start  justify-center items-center">
 					<h1 className="text-3xl text-white font-bold mb-4">Vite + React</h1>
 					<div className="flex space-x-2">
-						<button
+						<a
 							className="bg-black bg-opacity-35 rounded-md px-3 py-2 text-white font-semibold"
-							onClick={() => setCount((count) => count + 1)}
+							href="/#project-preview"
 						>
 							Resume
-						</button>
-						<button
+						</a>
+						<a
 							className="bg-white bg-opacity-45 rounded-md px-3 py-2 font-semibold"
-							onClick={() => setCount((count) => count + 1)}
+							href="/#project-preview"
 						>
 							Projects
-						</button>
+						</a>
 					</div>
 				</section>
-				<section className="h-screen snap-start">
-					<h1>Projects</h1>
-					<div className="flex">
+				<section className="h-screen snap-start flex" id="project-preview">
+					<div className="flex snap-x h-100">
+						<div className="w-100"></div>
+					</div>
+					<a
+						href="/"
+						className="absolute bottom-4 right-4 bg-blue-900 bg-opacity-35 border-2 border-blue-900 rounded-md px-3 py-2 font-semibold"
+					>
+						Back to Top
+					</a>
+				</section>
+				<section className="h-screen snap-start" id="projects">
+					<div className="flex overflow-y-auto">
+						<h1>Projects</h1>
 						<button onClick={() => setCount((count) => count + 1)}>
 							Resume {count}
 						</button>
@@ -36,9 +47,6 @@ function App() {
 							Projects {count}
 						</button>
 					</div>
-					<p>
-						Edit <code>src/App.jsx</code> and save to test HMR
-					</p>
 				</section>
 			</div>
 		</div>
