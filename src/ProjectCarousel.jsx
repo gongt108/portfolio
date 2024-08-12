4277aa34387a5a5bc651553f5fa4af412ade0c62
Tiffany Gong <gongt108@gmail.com>
2024-08-11 21:17:17 -0700

    style buttons

--- src/ProjectCarousel.jsx
+++ src/ProjectCarousel.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import { GoDotFill } from 'react-icons/go';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 
 const tempFiles = [
 	{
@@ -29,26 +30,33 @@ function ProjectCarousel() {
 	const [currentProjectId, setCurrentProjectId] = useState(0);
 
 	return (
-		<div className="w-100 h-100 relative">
-			<h2 className="absolute text-white text-3xl font-bold top-4 left-1/2">
-				{tempFiles[currentProjectId].name}
-			</h2>
-			<img
-				src={tempFiles[currentProjectId].imgUrl}
-				alt={`${tempFiles[currentProjectId].name} image`}
-				className="z-10"
-			/>
-
-			<div className="flex mx-auto absolute bottom-4">
-				{tempFiles.map((file, i) => {
-					return (
-						<div key={i}>
-							<div>
-								<GoDotFill />
+		<div className="flex">
+			<div className="my-auto bg-white bg-opacity-30 border-4 border-white px-2 py-3">
+				<MdKeyboardArrowLeft color="white" size={32} />
+			</div>
+			<div className="w-100 h-100 relative">
+				<h2 className="absolute text-white text-3xl font-bold top-4 left-1/2">
+					{tempFiles[currentProjectId].name}
+				</h2>
+				<img
+					src={tempFiles[currentProjectId].imgUrl}
+					alt={`${tempFiles[currentProjectId].name} image`}
+					className="z-10"
+				/>
+				<div className="flex absolute left-1/2 bottom-8 z-20">
+					{tempFiles.map((file, i) => {
+						return (
+							<div key={i}>
+								<div>
+									<GoDotFill color="white" />
+								</div>
 							</div>
-						</div>
-					);
-				})}
+						);
+					})}
+				</div>
+			</div>
+			<div className="my-auto bg-white bg-opacity-30 border-4 border-white px-2 py-3">
+				<MdKeyboardArrowRight color="white" size={32} />
 			</div>
 		</div>
 	);
