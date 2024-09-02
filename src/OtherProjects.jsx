358316bff31ae0d0d58eb58af70c12db7903c501
Tiffany Gong <gongt108@gmail.com>
2024-09-02 11:05:31 -0700

    change header to grid

--- src/OtherProjects.jsx
+++ src/OtherProjects.jsx
@@ -15,9 +15,13 @@ function OtherProjects() {
 
 	return (
 		<div className="overflow-y-scroll w-full flex flex-col bg-white">
-			<h1 className="text-3xl text-center text-white bg-slate-700 pt-6 pb-4 fixed w-full">
-				Other Projects
-			</h1>
+			<div className="text-white bg-slate-700 pt-6 pb-4 fixed w-full grid grid-cols-2">
+				<h3 className="text-sm ps-4 align-middle items-center justify-center">
+					Back to Main Projects
+				</h3>
+
+				<h1 className="text-3xl text-center mx-auto">Other Projects</h1>
+			</div>
 			<div className="mt-24 mx-8 flex flex-wrap justify-center gap-4 ">
 				{projects.map((project, i) => {
 					return <OtherProjectsCard />;
