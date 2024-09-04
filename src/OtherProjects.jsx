b2e42ff675e5fd0398b7e45bd94c4887af5f1268
Tiffany Gong <gongt108@gmail.com>
2024-09-03 23:39:49 -0700

    link other projects back to main projects

--- src/OtherProjects.jsx
+++ src/OtherProjects.jsx
@@ -4,6 +4,8 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import projects from './assets/projects.json';
 import OtherProjectsCard from './components/OtherProjectsCard';
 
+import { FaAngleLeft } from 'react-icons/fa6';
+
 function OtherProjects() {
 	const [selectedProject, setSelectedProject] = useState(0);
 	const navigate = useNavigate();
@@ -15,12 +17,16 @@ function OtherProjects() {
 
 	return (
 		<div className="overflow-y-scroll w-full flex flex-col bg-white">
-			<div className="text-white bg-slate-700 pt-6 pb-4 fixed w-full grid grid-cols-2">
-				<h3 className="text-sm ps-4 align-middle items-center justify-center">
+			<div className="text-white bg-slate-700 pt-6 pb-4 fixed w-full relative">
+				<a
+					className="text-sm left-4 top-4 align-middle items-center justify-center absolute flex hover:cursor-pointer hover:underline"
+					href="/projects"
+				>
+					<FaAngleLeft />
 					Back to Main Projects
-				</h3>
+				</a>
 
-				<h1 className="text-3xl text-center mx-auto">Other Projects</h1>
+				<h1 className="text-3xl text-center">Other Projects</h1>
 			</div>
 			<div className="mt-24 mx-8 flex flex-wrap justify-center gap-4 ">
 				{projects.map((project, i) => {
